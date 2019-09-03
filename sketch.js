c4c310e9e6081449d3635f908ff02f197f0f7266
huumpf <nico@windwalk.de>
2019-09-03 23:53:00 +0200

    Pasted example

--- sketch.js
+++ sketch.js
@@ -1,22 +1,36 @@
-var canvas;
-var looping = true;
+var myRec = new p5.SpeechRec(); // new P5.SpeechRec object
+var myVoice = new p5.Speech(); // new P5.Speech object
+
+myRec.continuous = true; // do continuous recognition
+// myRec.interimResults = true; // allow partial recognition (faster, less accurate)
+
 
 function setup() {
-  canvas = createCanvas(window.innerWidth, window.innerHeight);
+	// graphics stuff:
+	createCanvas(800, 400);
+	background(255, 255, 255);
+	fill(0, 0, 0, 255);
+	// instructions:
+	textSize(32);
+	textAlign(CENTER);
+	text("say something", width / 2, height / 2);
+	myRec.onResult = showResult;
+	myRec.start();
+  
+  myVoice.speak("say something");
+
 }
 
 function draw() {
-  background(220);
+	// why draw when you can talk?
 }
 
-function keyTyped() {
-  if (key === " ") {
-    if (looping) {
-      noLoop();
-      looping = false;
-    } else {
-      loop();
-      looping = true;
-    }
-  }
+function showResult() {
+	if (myRec.resultValue === true) {
+		background(255);
+		text(myRec.resultString, width / 2, height / 2);
+    //myVoice.interrupt = checkbox.elt.checked;
+		myVoice.speak(myRec.resultString); // debug printer for voice options
+		console.log(myRec.resultString);
+	}
 }
